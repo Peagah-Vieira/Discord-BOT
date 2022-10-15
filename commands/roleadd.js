22d77ee84df60a74e383c8481115b802e288d429
Peagah <peagahvieira2003@gmail.com>
2022-10-15 13:34:27 -0300

    Role_Add, Role_Remove with error handling

--- commands/roleadd.js
+++ commands/roleadd.js
@@ -1,3 +1,5 @@
+const { PermissionFlagsBits } = require("discord.js");
+
 module.exports = {
     name: 'roleadd',
     description: 'Atribua um Cargo à um Usuário',
@@ -13,16 +15,56 @@ module.exports = {
             type: 8, //'ROLE' Type
             description: 'Cargo que deseja atribuir.',
             required: true,
-        },
+        }
     ],
     async execute(interaction){
         const member = interaction.options.getMember('usuario')
         const cargo = interaction.options.getRole('cargo');
-        try {
-            await member.roles.add(cargo);
-        } 
-        catch(error){
-            console.log(`Erro: ${error}`);
+
+        if(!member.guild.members.me.permissions.has(PermissionFlagsBits.ManageRoles)){
+            const permissionErrorEmbed = {
+                color: 0xff0000, //VERMELHO
+                description: "Ops! Não tenho permissão para adicionar/remover cargos!"
+            }
+            return void interaction.reply({
+                embeds: [permissionErrorEmbed],
+                ephemeral: true
+            });
+        }
+
+        else if(member.guild.members.me.roles.highest.position <= cargo.position){
+            const errorEmbed = {
+                color: 0xff0000, //VERMELHO
+                description: "Não consigo atribuir membros à esse cargo! Talvez seja um cargo maior do que o meu?"
+            }
+            return void interaction.reply({
+                embeds: [errorEmbed],
+                ephemeral: true
+            });
+        }
+
+        else if(member.roles.cache.has(cargo.id)){
+            const alreadyRoleEmbed = {
+                color: 0xff0000, //VERMELHO
+                description: `Não foi possivel atribuir **${cargo}** à ${member.user}!`
+            }
+            return void interaction.reply({
+                embeds: [alreadyRoleEmbed],
+                ephemeral: true
+            });
+        }
+
+        else if(!member.roles.cache.has(cargo.id)){
+            const RoleEmbed = {
+                color: 0x0ae50a, // VERDE
+                description: `**${cargo}** foi atribuido à ${member.user}!`
+            }
+            member.roles.add(cargo)
+            .then( () => 
+            interaction.reply({
+                embeds: [RoleEmbed],
+                ephemeral: true
+            }));
         }
     }
 }
--- commands/roleremove.js
+++ commands/roleremove.js
@@ -1,3 +1,5 @@
+const { PermissionFlagsBits } = require("discord.js");
+
 module.exports = {
     name: 'roleremove',
     description: 'Remova um Cargo de um Usuário',
@@ -17,12 +19,51 @@ module.exports = {
     ],
     async execute(interaction){
         const member = interaction.options.getMember('usuario')
-        const cargo = interaction.options.getRole('cargo');
-        try {
-            await member.roles.remove(cargo);
-        } 
-        catch(error){
-            console.log(`Erro: ${error}`);
+        const role = interaction.options.getRole('cargo');
+
+        if(!member.guild.members.me.permissions.has(PermissionFlagsBits.ManageRoles)){
+            const permissionErrorEmbed = {
+                color: 0xff0000, //VERMELHO
+                description: "Ops! Não tenho permissão para adicionar/remover cargos!"
+            }
+            return void interaction.reply({
+                embeds: [permissionErrorEmbed],
+                ephemeral: true
+            });
+        }
+
+        else if(member.guild.members.me.roles.highest.position <= role.position){
+            const rolePositionErrorEmbed = {
+                color: 0xff0000, //VERMELHO
+                description: "Não consigo remover membros desse cargo! Talvez seja um cargo maior que o meu?"
+            }
+            return void interaction.reply({
+                embeds: [rolePositionErrorEmbed],
+                ephemeral: true
+            });
+        }
+    
+        else if(member.roles.cache.has(role.id)){
+            const removeRoleEmbed = {
+                color: 0x0ae50a, // VERDE
+                description: `**${role}** foi removido de ${member.user}!`
+            }
+            member.roles.remove(role).then( () =>
+            interaction.reply({
+                embeds: [removeRoleEmbed],
+                ephemeral: true
+            }));
+        }
+
+        else if(!member.roles.cache.has(role.id)){
+            const withoutRoleEmbed = {
+                color: 0xff0000, //VERMELHO
+                description: `Não foi possivel remover **${role}** de ${member.user}!`
+            }
+            return void interaction.reply({
+                embeds: [withoutRoleEmbed],
+                ephemeral: true
+            });
         }
     }
 }
