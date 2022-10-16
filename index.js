954cf4eac9d0caaaf7a18a1238346af152403eea
Peagah <peagahvieira2003@gmail.com>
2022-10-15 23:14:36 -0300

    Features: Auto_Role with Error_Handling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { Client, Collection, GatewayIntentBits, time, PermissionsBitField } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, PermissionFlagsBits, time } = require('discord.js');
 const ytdl = require('ytdl-core');
 const { Player } = require("discord-player");
 const { token} = require('./config.json');
@@ -99,42 +99,57 @@ client.on("guildDelete", (guild) => {
 });
 
 client.on("guildMemberAdd", (member) => {
-    const role = member.guild.roles.cache.find(role => role.name === "Membro");
+  
+  if(!member.guild.members.me.permissions.has(PermissionFlagsBits.ManageRoles)){
+    const errorChannel = member.guild.channels.cache.get("1029848898233180222");
+    const permissionErrorEmbed = {
+        color: 0xff0000, //VERMELHO
+        description: "Ops! Não tenho permissão para adicionar/remover cargos!"
+    }
+
+    return void errorChannel.send({
+      embeds: [permissionErrorEmbed],
+    });
+  }
+
+  else if(member.guild.members.me.permissions.has(PermissionFlagsBits.ManageRoles)){
+    const role = member.guild.roles.cache.find( (role) => role.name === "Membro");
+    const welcomeChannel = member.guild.channels.cache.get("1029848897633394747");
     const welcomeEmbed = {
-      color: 0x0ae50a, // VERDE
-      title: `Boas-Vindas`,
-      author: {
-          name: `${member.user.tag}`,
-          icon_url: member.user.avatarURL(),
-          url: member.user.avatarURL(),
-      },
-      description: `${member.user} Boas-Vindas ao Servidor **${member.guild.name}**, você recebeu a tag **${role}**! Não deixe de checar o canal <#1029848897633394742>! 😄`,
-      image:{
-        url: `https://media4.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif?cid=ecf05e476lz7l2x4cifar5r8spe3b125fglrg4b7w4z0bb89&rid=giphy.gif&ct=g`,
-        height: 0,
-        width: 0
-      },
-      thumbnail: {
-        url: `${member.user.displayAvatarURL({
-          dynamic: true,
-          format: "png",
-          size: 1024
-        })}`
-      },
-      footer: {
-        text: `ID do usuário: ${member.user.id}`,
-      }
+        color: 0x0ae50a, // VERDE
+        title: `Boas-Vindas`,
+        author: {
+            name: `${member.user.tag}`,
+            icon_url: member.user.avatarURL(),
+            url: member.user.avatarURL(),
+        },
+        description: `${member.user} Boas-Vindas ao Servidor **${member.guild.name}**, você recebeu a tag **${role}**! Não deixe de checar o canal <#1029848897633394742>! 😄`,
+        image:{
+          url: `https://media4.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif?cid=ecf05e476lz7l2x4cifar5r8spe3b125fglrg4b7w4z0bb89&rid=giphy.gif&ct=g`,
+          height: 0,
+          width: 0
+        },
+        thumbnail: {
+          url: `${member.user.displayAvatarURL({
+            dynamic: true,
+            format: "png",
+            size: 1024
+          })}`
+        },
+        footer: {
+          text: `ID do usuário: ${member.user.id}`,
+        }
     }
-    try{
-      member.roles.add(role);
-      member.guild.channels.cache.get("1029848897633394747").send({ 
+
+    member.roles.add(role).then( () => {
+      welcomeChannel.send({ 
         embeds: [welcomeEmbed],
-      });
-    }
-    catch (erro){
-      console.log(erro);
-      member.guild.channels.cache.get("1029848898233180222").send('Ocorreu um erro ao enviar a mensagem de boas-vindas!'); //BOT ERROS CHANNEL
-    }
+      })
+    }).catch( (erro) => {
+      errorChannel.send(`Ocorreu o seguinte erro ${erro} ao enviar a mensagem de boas-vindas!`);
+    });
+  }
+
 }); 
 
 client.on("guildMemberRemove", (member) => {
